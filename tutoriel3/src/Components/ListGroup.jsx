3a82fa40d975863db75b17f4ecdcbe1397aeb1d0
Amine <mkaouaramine83@gmail.com>
2024-02-21 09:41:44 +0100

    test3

--- tutoriel3/src/Components/ListGroup.jsx
+++ tutoriel3/src/Components/ListGroup.jsx
@@ -1,11 +1,8 @@
 function ListGroup() {
   const villes = ['Tunis', 'Sousse', 'Touzeur', 'Sfax', 'Sidi Bouzid', 'Tataouine'];
   const startWith = "S"; 
-
   const Svilles = villes.filter(ville => ville.startsWith(startWith));
-
   const sVilles = Svilles.map((ville) => <li key={ville.id}>{ville}</li>);
-
   return (
     <ul>
       {sVilles}
